61cc723bbec364159938e83891c3061f82af5bd1
Thor138 <tjcourey84@gmail.com>
2021-06-24 14:21:40 -0600

    added header and footer

--- src/components/header-footer/footer.js
+++ src/components/header-footer/footer.js
@@ -2,11 +2,11 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="footer">
+    <footer className="footer">
       <a href="mailto:tjcourey84@gmail.com">TJCourey84@Gmail.com</a>
       <a href="https://www.linkedin.com/in/tj-courey-8817b1208/">LinkedIn </a>
       <a href="https://github.com/TJCourey">GitHub</a>
-    </div>
+    </footer>
   );
 };
 
--- src/components/header-footer/header.js
+++ src/components/header-footer/header.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Header() {
-  return <></>;
+  return <header>PICTURE BACKGROUND Name Text</header>;
 }
 
 export default Header;
