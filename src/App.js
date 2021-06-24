e86a587be9ccec72d38eb29d59f90c6530289d75
Thor138 <tjcourey84@gmail.com>
2021-06-24 10:02:40 -0600

    added header and footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import NavHandler from "./components/navHandler";
+import Footer from "./components/header-footer/footer";
+import Header from "./components/header-footer/header";
 
 function App() {
   return (
     <div>
+      <Header />
       <NavHandler />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/header-footer/footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <a href="mailto:tjcourey84@gmail.com">TJCourey84@Gmail.com</a>
+      <a href="https://www.linkedin.com/in/tj-courey-8817b1208/">LinkedIn </a>
+      <a href="https://github.com/TJCourey">GitHub</a>
+    </div>
+  );
+};
+
+export default Footer;
