6bc2d05eeef46b2bcccf833eaf45975bad92ec3c
Thor138 <tjcourey84@gmail.com>
2021-07-09 20:13:53 -0600

    added gradient color to background

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import Header from "./components/header-footer/header";
 
 function App() {
   return (
-    <div fluid style={{ backgroundImage: "url(../images/greyBackground.png)" }}>
+    <div
+      style={{
+        background: " rgb(204,204,205)",
+        background:
+          "linear-gradient(180deg, rgba(204,204,205,1) 0%, rgba(159,159,166,1) 35%, rgba(77,78,78,1) 100%)",
+        paddingBottom: "2rem",
+      }}
+    >
       <Header />
       <NavHandler />
       <Footer />
